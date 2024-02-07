2293ab374d89b434aeaee08e929600735007d23f
Saurabhkasabe <156282037+Saurabhkasabe@users.noreply.github.com>
2024-02-07 14:47:37 +0530

    Add files via upload

---
+++ OddEvenNumber.java
@@ -0,0 +1,18 @@
+package week2.day1;
+
+public class OddEvenNumber {
+
+	public static void main(String[] args) {
+		 
+		int n=16;
+		
+	
+		if(n%2==1) {
+			System.out.println("The given number is odd");
+		}else {
+			System.out.println("The given number is even");
+		}
+
+	}
+
+}
---
+++ WhileLoop.java
@@ -0,0 +1,15 @@
+package week2.day1;
+
+public class WhileLoop {
+
+	public static void main(String[] args) {
+           
+		
+		int i=10;
+		while(i>1) { 
+			System.out.println(i);
+			i--;
+		}
+	}
+
+}
